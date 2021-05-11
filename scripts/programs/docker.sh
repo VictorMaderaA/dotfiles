235eb80b3c2262f790e0a653b7bb84149bdd55d0
VictorMaderaA <victor.madera.a@gmail.com>
2021-05-11 11:26:42 +0200

    [add] Docker Compose Install

--- scripts/programs/docker.sh
+++ scripts/programs/docker.sh
@@ -16,4 +16,8 @@ sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
 
+# TODO - 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 echo "Executed docker.sh"
