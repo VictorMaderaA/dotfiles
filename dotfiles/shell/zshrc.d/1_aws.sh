026cf4e19390b05f4f7794c27213e37bdb801458
VictorMaderaA <victor.madera.a@gmail.com>
2025-12-08 19:18:58 +0100

    feat(shell): Enable AWS CLI autocompletion in `.zshrc` [main]
    Added AWS CLI autocompletion setup to `.zshrc.d/1_aws.sh` using `bashcompinit` and `aws_completer`. This improves the command-line experience for AWS CLI users.

--- dotfiles/shell/zshrc.d/1_aws.sh
+++ dotfiles/shell/zshrc.d/1_aws.sh
@@ -1,1 +1,6 @@
 export PATH="$PATH:$HOME/.local/bin/git-remote-codecommit"
+
+# Enable AWS CLI autocompletion
+autoload -Uz compinit && compinit
+autoload -Uz bashcompinit && bashcompinit
+complete -C '/usr/local/bin/aws_completer' aws
